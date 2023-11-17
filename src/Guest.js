3300aee1ee52a9ca4f98377d2ff7828919d03faf
Earl Mike Sarabia <80509164+esmike03@users.noreply.github.com>
2023-11-17 22:58:51 +0800

    Add files via upload

---
+++ src/Guest.js
@@ -0,0 +1,274 @@
+const DATA = [
+    {
+      id: 0,
+      name: { 
+        fname: "Arnold James",
+        lname: "Felisilda"
+      },
+      nick: "Nold",
+      course: "BSIT - 3A"
+  },
+  {
+      id: 1,
+      name: { 
+        fname: "Nicole James",
+        lname: "Paguican"
+      },
+      nick: "Nics",
+      course: "BSIT - 3A"
+  },
+  {
+      id: 2,
+      name: { 
+        fname: "Rodel",
+        lname: "Garcia"
+      },
+      nick: "Rodel",
+      course: "BSIT - 3A"
+  },
+  {
+      id: 3,
+      name: { 
+        fname: "Adrian Jane",
+        lname: "Tahil"
+      },
+      nick: "Ja Ne",
+      course: "BSIT - 3A"
+  },
+  {
+      id: 4,
+      name: { 
+        fname: "Joana",
+        lname: "Jagunos"
+      },
+      nick: "Joaney",
+      course: "BSIT - 3A"
+  },
+  {
+      id: 5,
+      name: { 
+        fname: "Mic Roland",
+        lname: "Mirabite"
+      },
+      nick: "Mic",
+      course: "BSIT - 3A"
+  },
+  {
+      id: 6,
+      name: { 
+        fname: "Marlon",
+        lname: "Carbonilla"
+      },
+      nick: "Marlonskie",
+      course: "BSIT - 3A"
+  },
+  {
+      id: 7,
+      name: { 
+        fname: "John",
+        lname: "Tabanyag"
+      },
+      nick: "Johnny",
+      course: "BSIT - 3A"
+  },
+  {
+    id: 8,
+    name: { 
+      fname: "Kenneth",
+      lname: "Tadlas"
+    },
+    nick: "Kenneth",
+    course: "BSIT - 3A"
+  },
+  {
+    id: 9,
+    name: { 
+      fname: "Nelbrey Jillian",
+      lname: "Escuadro"
+    },
+    nick: "Nelbrey",
+    course: "BSIT - 3A"
+  },
+  {
+    id: 10,
+    name: { 
+      fname: "Warren",
+      lname: "Bucia"
+    },
+    nick: "War",
+    course: "BSIT - 3A"
+  },
+  {
+    id: 11,
+    name: { 
+      fname: "Sherline",
+      lname: "Dondoyano"
+    },
+    nick: "Sherline",
+    course: "BSIT - 3A"
+  },
+  {
+    id: 12,
+    name: { 
+      fname: "Airen Mae",
+      lname: "Bongcaras"
+    },
+    nick: "Airen",
+    course: "BSIT - 3A"
+  },
+  {
+    id: 13,
+    name: { 
+      fname: "Earl Mike",
+      lname: "Sarabia"
+    },
+    nick: "Earl Mike",
+    course: "BSIT - 3A"
+  },
+  {
+    id: 14,
+    name: { 
+      fname: "Ira Jane",
+      lname: "Renoblas"
+    },
+    nick: "Ira Janey",
+    course: "BSIT - 3A"
+  },
+  {
+    id: 15,
+    name: { 
+      fname: "Ria Mae",
+      lname: "Angcahan"
+    },
+    nick: "Ria",
+    course: "BSIT - 3A"
+},
+{
+    id: 16,
+    name: { 
+      fname: "Heshiel Jay",
+      lname: "Juntilla"
+    },
+    nick: "Heshy",
+    course: "BSIT - 3A"
+},
+{
+    id: 17,
+    name: { 
+      fname: "Angelo",
+      lname: "Maldora"
+    },
+    nick: "Gelo",
+    course: "BSIT - 3A"
+},
+{
+    id: 18,
+    name: { 
+      fname: "Arnell",
+      lname: "Pajota"
+    },
+    nick: "Nell",
+    course: "BSIT - 3A"
+},
+{
+    id: 19,
+    name: { 
+      fname: "Russel",
+      lname: "Mandanguit"
+    },
+    nick: "Rus",
+    course: "BSIT - 3A"
+},
+{
+    id: 20,
+    name: { 
+      fname: "Jesriel",
+      lname: "Pilipino"
+    },
+    nick: "Jesriel",
+    course: "BSIT - 3A"
+},
+{
+    id: 21,
+    name: { 
+      fname: "Jesriel",
+      lname: "Linog"
+    },
+    nick: "Jesriel",
+    course: "BSIT - 3A"
+},
+{
+    id: 22,
+    name: { 
+      fname: "Jesse",
+      lname: "Amang"
+    },
+    nick: "Jess",
+    course: "BSIT - 3A"
+},
+{
+  id: 23,
+  name: { 
+    fname: "Kent Ivan",
+    lname: "Amora"
+  },
+  nick: "Keny",
+  course: "BSIT - 3A"
+},
+{
+  id: 24,
+  name: { 
+    fname: "Bernadette",
+    lname: "Banaga"
+  },
+  nick: "Bern",
+  course: "BSIT - 3A"
+},
+{
+  id: 25,
+  name: { 
+    fname: "Sheilo",
+    lname: "Saraga"
+  },
+  nick: "Sheil",
+  course: "BSIT - 3A"
+},
+{
+  id: 26,
+  name: { 
+    fname: "Raymond",
+    lname: "Bunol"
+  },
+  nick: "Mond",
+  course: "BSIT - 3A"
+},
+{
+  id: 27,
+  name: { 
+    fname: "Sherwen",
+    lname: "Lahoy"
+  },
+  nick: "Sherwen",
+  course: "BSIT - 3A"
+},
+{
+  id: 28,
+  name: { 
+    fname: "Rhizabell",
+    lname: "Dalen"
+  },
+  nick: "Rhizabell",
+  course: "BSIT - 3A"
+},
+{
+  id: 29,
+  name: { 
+    fname: "Pearly Diane",
+    lname: "Magale"
+  },
+  nick: "Pearly",
+  course: "BSIT - 3A"
+},
+  ];
+
+  export default DATA;
